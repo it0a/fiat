8c98ba4e6f9397c0d6ae2bcde404b12dc242b051
it0a <recursive404@gmail.com>
2014-11-19 22:13:11 -0500

    Adding some improved output, getting ready to actually capture and record inputs

--- fiat.go
+++ fiat.go
@@ -1,13 +1,240 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/codegangsta/cli"
 	"log"
 	"os"
+	"os/signal"
 )
 
-var CodeMap = map[uint32]string{
+type Event struct {
+	Code  byte
+	Value byte
+}
+
+var FreqMap = map[byte]uint32{
+	1:   0,
+	2:   0,
+	3:   0,
+	4:   0,
+	5:   0,
+	6:   0,
+	7:   0,
+	8:   0,
+	9:   0,
+	10:  0,
+	11:  0,
+	12:  0,
+	13:  0,
+	14:  0,
+	15:  0,
+	16:  0,
+	17:  0,
+	18:  0,
+	19:  0,
+	20:  0,
+	21:  0,
+	22:  0,
+	23:  0,
+	24:  0,
+	25:  0,
+	26:  0,
+	27:  0,
+	28:  0,
+	29:  0,
+	30:  0,
+	31:  0,
+	32:  0,
+	33:  0,
+	34:  0,
+	35:  0,
+	36:  0,
+	37:  0,
+	38:  0,
+	39:  0,
+	40:  0,
+	41:  0,
+	42:  0,
+	43:  0,
+	44:  0,
+	45:  0,
+	46:  0,
+	47:  0,
+	48:  0,
+	49:  0,
+	50:  0,
+	51:  0,
+	52:  0,
+	53:  0,
+	54:  0,
+	55:  0,
+	56:  0,
+	57:  0,
+	58:  0,
+	59:  0,
+	60:  0,
+	61:  0,
+	62:  0,
+	63:  0,
+	64:  0,
+	65:  0,
+	66:  0,
+	67:  0,
+	68:  0,
+	69:  0,
+	70:  0,
+	71:  0,
+	72:  0,
+	73:  0,
+	74:  0,
+	75:  0,
+	76:  0,
+	77:  0,
+	78:  0,
+	79:  0,
+	80:  0,
+	81:  0,
+	82:  0,
+	83:  0,
+	85:  0,
+	86:  0,
+	87:  0,
+	88:  0,
+	89:  0,
+	90:  0,
+	91:  0,
+	92:  0,
+	93:  0,
+	94:  0,
+	95:  0,
+	96:  0,
+	97:  0,
+	98:  0,
+	99:  0,
+	100: 0,
+	101: 0,
+	102: 0,
+	103: 0,
+	104: 0,
+	105: 0,
+	106: 0,
+	107: 0,
+	108: 0,
+	109: 0,
+	110: 0,
+	111: 0,
+	112: 0,
+	113: 0,
+	114: 0,
+	115: 0,
+	116: 0,
+	117: 0,
+	118: 0,
+	119: 0,
+	121: 0,
+	122: 0,
+	123: 0,
+	124: 0,
+	125: 0,
+	126: 0,
+	127: 0,
+	128: 0,
+	129: 0,
+	130: 0,
+	131: 0,
+	132: 0,
+	133: 0,
+	134: 0,
+	135: 0,
+	136: 0,
+	137: 0,
+	138: 0,
+	139: 0,
+	140: 0,
+	141: 0,
+	142: 0,
+	143: 0,
+	144: 0,
+	145: 0,
+	146: 0,
+	147: 0,
+	148: 0,
+	149: 0,
+	150: 0,
+	151: 0,
+	152: 0,
+	153: 0,
+	154: 0,
+	155: 0,
+	156: 0,
+	157: 0,
+	158: 0,
+	159: 0,
+	160: 0,
+	161: 0,
+	162: 0,
+	163: 0,
+	164: 0,
+	165: 0,
+	166: 0,
+	167: 0,
+	168: 0,
+	169: 0,
+	170: 0,
+	171: 0,
+	172: 0,
+	173: 0,
+	174: 0,
+	175: 0,
+	176: 0,
+	177: 0,
+	178: 0,
+	179: 0,
+	180: 0,
+	183: 0,
+	184: 0,
+	185: 0,
+	186: 0,
+	187: 0,
+	188: 0,
+	189: 0,
+	190: 0,
+	191: 0,
+	192: 0,
+	193: 0,
+	194: 0,
+	200: 0,
+	201: 0,
+	202: 0,
+	203: 0,
+	205: 0,
+	206: 0,
+	207: 0,
+	208: 0,
+	209: 0,
+	210: 0,
+	211: 0,
+	212: 0,
+	213: 0,
+	214: 0,
+	215: 0,
+	216: 0,
+	217: 0,
+	218: 0,
+	219: 0,
+	220: 0,
+	221: 0,
+	222: 0,
+	223: 0,
+	224: 0,
+	225: 0,
+	226: 0,
+	240: 0,
+}
+
+var CodeMap = map[byte]string{
 	1:   "ESC",
 	2:   "1",
 	3:   "2",
@@ -233,58 +460,62 @@ var ValueMap = map[int32]string{
 	2: "HOLDING",
 }
 
-type Input_Event struct {
-	Code  uint32
-	Value uint32
-}
-
 func main() {
 	app := cli.NewApp()
 	app.Name = "fiat"
 	app.Usage = "free input analysis tool"
 	app.Version = "0.0.1"
-	app.Commands = []cli.Command{
-		{
-			Name:      "capture",
-			ShortName: "c",
-			Usage:     "capture input",
-			Action: func(c *cli.Context) {
-				fmt.Println("TODO")
-			},
-		},
-	}
 	app.Action = func(c *cli.Context) {
-		testHook()
+		listen()
 	}
 	app.Run(os.Args)
 }
 
-func DecodeEvent(buf []byte, event *Input_Event) {
-	event.Code = uint32(buf[18]) + uint32(buf[19])<<8
-	event.Value = uint32(buf[20]) + uint32(buf[21])<<8
+func DecodeEvent(buf []byte, ev *Event) {
+	ev.Code = buf[18]
+	ev.Value = buf[20]
 }
 
-func ReadEvent(f *os.File) Input_Event {
-	var event Input_Event
+func ReadEvent(f *os.File) Event {
+	var ev Event
 	buf := make([]byte, 24)
 	n, err := f.Read(buf)
 	if err != nil {
 		log.Fatal(n, err)
 	}
-	DecodeEvent(buf, &event)
-	return event
+	DecodeEvent(buf, &ev)
+	return ev
 }
 
-func testHook() {
+func listen() {
 	kbd, err := os.Open("/dev/input/event0")
-	if err != nil {
-		panic(err)
-	}
 	defer kbd.Close()
-	for {
-		event := ReadEvent(kbd)
-		if event.Code >= 1 && event.Code <= 240 && event.Value == 1 {
-			fmt.Print(CodeMap[event.Code])
+	tmp, err := os.Create("/tmp/fiat")
+	defer tmp.Close()
+	w := bufio.NewWriter(tmp)
+	if err != nil {
+		println(err.Error())
+	} else {
+		attachInterrupt()
+		for {
+			ev := ReadEvent(kbd)
+			if ev.Code >= 1 && ev.Code <= 240 && ev.Value == 1 {
+				FreqMap[ev.Code]++
+				fmt.Printf("%v => %v\n", CodeMap[ev.Code], FreqMap[ev.Code])
+				w.WriteByte(ev.Code)
+			}
+			w.Flush()
 		}
 	}
+}
+
+func attachInterrupt() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Printf("Captured %v, stopping data capture", sig)
+			os.Exit(1)
+		}
+	}()
 }
