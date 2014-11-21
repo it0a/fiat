5065cc2a7c1fb5479f4c87cb5dfd5feb9ed3a8c3
it0a <recursive404@gmail.com>
2014-11-20 21:52:34 -0500

    Adding stat function

--- fiat.go
+++ fiat.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
 	"github.com/codegangsta/cli"
 	"log"
@@ -15,6 +16,133 @@ type Event struct {
 }
 
 var BufWriter bufio.Writer
+
+var FreqStatMap = map[byte]uint32{
+	1:   0,
+	2:   0,
+	3:   0,
+	4:   0,
+	5:   0,
+	6:   0,
+	7:   0,
+	8:   0,
+	9:   0,
+	10:  0,
+	11:  0,
+	12:  0,
+	13:  0,
+	14:  0,
+	15:  0,
+	16:  0,
+	17:  0,
+	18:  0,
+	19:  0,
+	20:  0,
+	21:  0,
+	22:  0,
+	23:  0,
+	24:  0,
+	25:  0,
+	26:  0,
+	27:  0,
+	28:  0,
+	29:  0,
+	30:  0,
+	31:  0,
+	32:  0,
+	33:  0,
+	34:  0,
+	35:  0,
+	36:  0,
+	37:  0,
+	38:  0,
+	39:  0,
+	40:  0,
+	41:  0,
+	42:  0,
+	43:  0,
+	44:  0,
+	45:  0,
+	46:  0,
+	47:  0,
+	48:  0,
+	49:  0,
+	50:  0,
+	51:  0,
+	52:  0,
+	53:  0,
+	54:  0,
+	55:  0,
+	56:  0,
+	57:  0,
+	58:  0,
+	59:  0,
+	60:  0,
+	61:  0,
+	62:  0,
+	63:  0,
+	64:  0,
+	65:  0,
+	66:  0,
+	67:  0,
+	68:  0,
+	69:  0,
+	70:  0,
+	71:  0,
+	72:  0,
+	73:  0,
+	74:  0,
+	75:  0,
+	76:  0,
+	77:  0,
+	78:  0,
+	79:  0,
+	80:  0,
+	81:  0,
+	82:  0,
+	83:  0,
+	85:  0,
+	86:  0,
+	87:  0,
+	88:  0,
+	89:  0,
+	90:  0,
+	91:  0,
+	92:  0,
+	93:  0,
+	94:  0,
+	95:  0,
+	96:  0,
+	97:  0,
+	98:  0,
+	99:  0,
+	100: 0,
+	101: 0,
+	102: 0,
+	103: 0,
+	104: 0,
+	105: 0,
+	106: 0,
+	107: 0,
+	108: 0,
+	109: 0,
+	110: 0,
+	111: 0,
+	112: 0,
+	113: 0,
+	114: 0,
+	115: 0,
+	116: 0,
+	117: 0,
+	118: 0,
+	119: 0,
+	121: 0,
+	122: 0,
+	123: 0,
+	124: 0,
+	125: 0,
+	126: 0,
+}
 
 var FreqMap = map[byte]byte{
 	1:   0,
@@ -279,14 +407,14 @@ func ReadEvent(f *os.File) Event {
 	return ev
 }
 
-func flushFrequencyMap() []byte {
+func flushFrequencyMap(freqMap map[byte]byte) []byte {
 	buf := []byte{}
-	for key := range FreqMap {
-		if FreqMap[key] > 0 {
-			fmt.Printf("Writing key %s %v times\n", CodeMap[key], FreqMap[key])
+	for key := range freqMap {
+		if freqMap[key] > 0 {
+			fmt.Printf("Writing key %s %v times\n", CodeMap[key], freqMap[key])
 			buf = append(buf, key)
-			buf = append(buf, FreqMap[key])
-			FreqMap[key] = 0
+			buf = append(buf, freqMap[key])
+			freqMap[key] = 0
 		}
 	}
 	return buf
@@ -319,7 +447,7 @@ func listen() {
 		if ev.Code >= 1 && ev.Code <= 126 && ev.Value == 1 {
 			FreqMap[ev.Code] = FreqMap[ev.Code] + 1
 			if FreqMap[ev.Code] == 8 {
-				BufWriter.Write(flushFrequencyMap())
+				BufWriter.Write(flushFrequencyMap(FreqMap))
 			}
 		}
 	}
@@ -341,11 +469,43 @@ func attachInterrupt() {
 	}()
 }
 
+func viewStatistics() {
+	statFile, err := os.Open("/tmp/fiat")
+	if err != nil {
+		log.Fatal("Couldn't open /tmp/fiat! ", err)
+	}
+	BufReader := bufio.NewReader(statFile)
+	for {
+		code, err := BufReader.ReadByte()
+		freq, err := BufReader.ReadByte()
+		if err != nil {
+			fmt.Println("EOF")
+			break
+		}
+		FreqStatMap[code] = FreqStatMap[code] + binary.LittleEndian.Uint32([]byte{freq, 0, 0, 0})
+	}
+	for key := range FreqStatMap {
+		if FreqStatMap[key] > 0 {
+			fmt.Printf("Key/Freq: %s => %v\n", CodeMap[key], FreqStatMap[key])
+		}
+	}
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "fiat"
 	app.Usage = "free input analysis tool"
 	app.Version = "0.0.1"
+	app.Commands = []cli.Command{
+		{
+			Name:      "stat",
+			ShortName: "st",
+			Usage:     "view current statistics",
+			Action: func(c *cli.Context) {
+				viewStatistics()
+			},
+		},
+	}
 	app.Action = func(c *cli.Context) {
 		listen()
 	}
